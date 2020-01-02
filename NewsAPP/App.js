d0d3536373551e2e6a99224a757e095f87bc7456
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-01-02 15:46:15 +0530

    In the process

--- NewsAPP/App.js
+++ NewsAPP/App.js
@@ -1,11 +1,106 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Container, Header, Content, Tab, Tabs,Left,Right,Body,Title,ListItem,List,Button,Thumbnail } from 'native-base';
+
+const articlr_url = ''
+const country_code = ''
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello My name is Rishabh Sharma</Text>
-    </View>
+    <Container >
+  
+   
+    <Header hasTabs style={{backgroundColor : '#009387' , alignItems : 'center' , justifyContent : 'center'}}>
+      <Left/>
+      <Body style={{justifyContent: 'center',alignItems : 'center'}}>
+        <Title>Global News</Title>
+      </Body>
+      <Right />
+    </Header>
+    
+    <Tabs tabBarUnderlineStyle = {{backgroundColor : 'yellow'}}>
+      <Tab heading="Tab1" activeTabStyle={{backgroundColor :  '#009387'}}>
+      <Container>
+        
+        <Content>
+          <List>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={{ uri: 'Image URL' }} />
+              </Left>
+              <Body>
+                <Text>Sankhadeep</Text>
+                <Text note numberOfLines={2}>Its time to build a difference and to build a divgefd . .</Text>
+              </Body>
+              <Right>
+              <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+      </Tab>
+
+
+
+
+      <Tab heading="Tab2" activeTabStyle={{backgroundColor :  '#009387'}}>
+      <Container>
+       
+        <Content>
+          <List>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={{ uri: 'Image URL' }} />
+              </Left>
+              <Body>
+                <Text>Sankhadeep</Text>
+                <Text note numberOfLines={2}>Its time to build a difference chuhehcuerrhceer. .</Text>
+              </Body>
+              <Right>
+              <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+      </Tab>
+
+
+      <Tab heading="Tab3" activeTabStyle={{backgroundColor :  '#009387'}}>
+      <Container>
+       
+        <Content>
+          <List>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={{ uri: 'Image URL' }} />
+              </Left>
+              <Body>
+                <Text>Sankhadeep</Text>
+                <Text note numberOfLines={2}>Its time to build a difference mnnenenneklmnkermk. .</Text>
+              </Body>
+              <Right>
+              <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+      </Tab>
+
+
+
+      
+    </Tabs>
+  </Container>
   );
 }
 
@@ -16,4 +111,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header : {
+    marginTop : 0
+  },
+  title : {
+    justifyContent : 'center',
+    alignContent : 'center'
+  }
 });
---
+++ NewsAPP/configurations/rest_config.js
@@ -0,0 +1,4 @@
+export const articles_url = "https://newsapi.org/v2/top-headlines"
+export const language = "en"
+export const cateogary = "general"
+export const api_key = "d316b74737fa452fab5439541498a642"
